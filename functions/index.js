87a145aad5a5124481d341c0d5aa2b468f015233
Gary Woodfine <gary.woodfine@threenine.co.uk>
2019-10-03 13:19:06 +0100

    Finished Function Implementation

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+
+admin.initializeApp();
+const db = admin.firestore();
+
+const createProfile = (userRecord, context) => {
+
+  const {email, uid} = userRecord;
+
+  return db
+    .collection('users')
+    .doc(uid)
+    .set({email})
+    .catch(console.error);
+
+};
+
+module.exports = {
+  authOnCreate: functions.auth.user().onCreate(createProfile),
+};
+
