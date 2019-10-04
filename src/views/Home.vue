3b460ff141e6df28fdcfaf76adc5bb753b64e4f5
Gary Woodfine <gary.woodfine@threenine.co.uk>
2019-10-04 09:56:01 +0100

    Tutorial post completed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,25 @@
         new things but also build a business at the same time. A business whose primary purpose is to put
         more hope, good and general well being in the world.
         </p>
-        <p>This concept is still in the very early stages and is an ever changing work in progress</p>
+        <p>This concept is still in the very early stages and is an ever changing work in progress.
+        It is entirely possible that this concept or latest brainwave may not actually get off the ground,
+        and like so many Software Developer side projects and ideas it will cease to exist and dissapear,
+        lost to the new shiny which will replace it.</p>
+        <p><b>For the time being, at least it lives!</b></p>
+        <p>One of the main inspirations for this site and the applications it will spawn, are the initial series
+        of tutorials developed to help developers get acquainted with JavaScript, Vue, .net Core, Ionic, Firebase and other
+        new and exciting technologies which take Gary's fancy</p>
+        <p>The initial set of tutorials can be found here :</p>
+        <ul>
+          <li><a href="https://garywoodfine.com/how-to-start-a-new-project-using-vue-js" target="_blank">How to start a new project using Vue.js</a></li>
+          <li><a href="Hhttps://garywoodfine.com/how-to-install-firebase-with-vue-js/" target="_blank">How to Install Firebase with Vue.JS</a></li>
+          <li><a href="https://garywoodfine.com/using-vuex-and-vue-router-in-a-vue-project/" target="_blank">How to Use Vuex and Vue-Router in a Vue.JS Project</a></li>
+          <li><a href="https://garywoodfine.com/how-to-implement-navigation-guards-vue/" target="_blank">How to implement Navigation Guards Vue</a></li>
+          <li><a href="https://garywoodfine.com/how-to-use-firebase-cloud-functions/" target="_blank">How to Use Firebase Cloud Functions</a></li>
+          <li><a href="https://garywoodfine.com/how-to-split-vuex-store-into-modules" target="_blank">How to split Vuex Store into modules</a></li>
+        </ul>
+
+        <p>The source code for this site and the associated tutorials are all in the <a href="https://github.com/threenine/sourcelink-web" target="blank">Github Repository</a> </p>
       </mdb-col>
    </mdb-row>
   </mdb-container>
--- src/views/user/Profile.vue
+++ src/views/user/Profile.vue
@@ -4,7 +4,6 @@
       <mdb-col sm="3">
         <mdb-card class="mt-3">
           <mdb-card-body>
-
             <mdb-file-input :src="profile.image" @getValue="saveImage" btn-title="upload"
                             circle icon="upload" img name="ProfileImage" rounded sm/>
           </mdb-card-body>
@@ -14,7 +13,6 @@
 
       <mdb-card class="mt-3 p-1">
         <mdb-card-body>
-
           <form @submit.prevent="saveProfile" novalidate>
             <mdb-list-group class="mb-4" v-if="errors.length">
               <mdb-list-group-item :key="error" class="list-group-item-danger"
@@ -41,7 +39,6 @@
                 </mdb-col>
               </mdb-row>
             </mdb-container>
-
             <div class="text-right">
               <mdb-btn size="sm" type="submit">Save</mdb-btn>
             </div>
@@ -67,9 +64,11 @@ import {
   mdbRow,
   mdbFileInput,
 } from 'mdbvue';
+
 import { Auth } from '@/firebase/auth';
 import { Storage } from '@/firebase/storage';
 import { DB } from '@/firebase/db';
+import validate from '@/modules/validation-module';
 
 export default {
   name: 'Profile',
@@ -108,11 +107,19 @@ export default {
         this.errors.push('A Firstname is required');
       }
 
+      if (this.profile.lastName === '') {
+        this.errors.push('A lastname is required');
+      }
+
+      if (!validate.email(this.email)) {
+        this.errors.push('Please enter a valid email address');
+      }
+
       if (!this.errors.length) {
         event.target.classList.add('was-validated');
-        const { uid } = Auth.currentUser;
+
         DB.collection('users')
-          .doc(uid)
+          .doc(Auth.currentUser.uid)
           .onSnapshot((doc) => {
             if (doc.exists) {
               doc.ref.update(this.profile)
@@ -134,21 +141,19 @@ export default {
     },
     saveImage(payload) {
       const file = payload[0];
-      const extension = file.name.split('.')
-        .pop();
-      const { currentUser } = Auth;
+      const extension = file.name.split('.').pop();
       const uploadDate = new Date();
-      const fileName = `${currentUser.uid}-${uploadDate.toDateString()}.${extension}`;
+      const fileName = `${Auth.currentUser.uid}-${uploadDate.toDateString()}.${extension}`;
 
       Storage.ref(`users/profile/${fileName}`)
         .put(file)
         .then((response) => {
           response.ref.getDownloadURL().then((link) => {
-            currentUser.updateProfile({
+            Auth.currentUser.updateProfile({
               photoURL: link,
             }).then(() => {
               DB.collection('users')
-                .doc(currentUser.uid)
+                .doc(Auth.currentUser.uid)
                 .get()
                 .then((doc) => {
                   if (doc.exists) {
@@ -166,3 +171,7 @@ export default {
 <style scoped>
 
 </style>
+https://amzn.to/2AHoVRH
+https://mdbootstrap.com/docs/vue/forms/file-input?utm_ref_id=47167
+
+https://mdbootstrap.com/docs/vue/?utm_ref_id=47167
