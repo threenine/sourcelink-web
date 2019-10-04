bceb5fe46d3fcca8b748e8cb1b43a7561e2777af
Gary Woodfine <gary.woodfine@threenine.co.uk>
2019-10-04 06:38:46 +0100

    Added some general intro text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,18 @@
 <template>
   <mdb-container class="mt-5 p-5">
     <mdb-row class="mt-5 p-5">
-      <mdb-col> <h1> This is the homepage</h1> </mdb-col>
+      <mdb-col>
+      <h1>Software Developer Community</h1>
+        <h2>Developed by Software Developers</h2>
+        <p>Searching for an need and outlet to develop a side project to have a play around with some
+        of the latest tools, services, platforms and ideas, <a href="https://garywoodfine.com" target="_blank">Gary Woodfine</a>
+        , was inspired to start develop this site and application to help build software developer community site.</p>
+        <p>If nothing else this site will provide an outlet and scratch an itch to not only continually learn
+        new things but also build a business at the same time. A business whose primary purpose is to put
+        more hope, good and general well being in the world.
+        </p>
+        <p>This concept is still in the very early stages and is an ever changing work in progress</p>
+      </mdb-col>
    </mdb-row>
   </mdb-container>
 </template>
