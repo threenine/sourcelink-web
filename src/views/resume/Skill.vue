77910b84142197fef36afffe1ddfef846fc942c5
Gary Woodfine <gary.woodfine@threenine.co.uk>
2019-10-14 11:50:59 +0100

    Finished GraphQL implementation

--- src/views/resume/Skill.vue
+++ src/views/resume/Skill.vue
@@ -9,17 +9,14 @@
             <mdb-card-title class="section-title">Programming Languages</mdb-card-title>
             <mdb-row>
               <mdb-col>
-                <mdb-autocomplete
-                  icon="list-ul"
-                  clearClass="fa-lg"
-                  class="wide-box"
-                  :data="languages"
-                  label="Languages"
-                  v-on:input="handler"
-                />
+                <mdb-select search v-model="languages"
+                            id="language"
+                            label="Language"
+                            @getvalue="selectedLanguage"/>
+
               </mdb-col>
               <mdb-col>
-               <mdb-btn @click="addLanguage" v-model="selected">
+               <mdb-btn @click="addLanguage">
                  <mdb-icon icon="plus"></mdb-icon>
                </mdb-btn>
               </mdb-col>
@@ -28,7 +25,7 @@
             <mdb-row>
               <mdb-col>
                 <mdb-list-group>
-                  <mdb-list-group-item v-for="language in selLanguage"
+                  <mdb-list-group-item v-for="language in selectedLanguages"
                                        v-bind:key="language">{{ language }}
                   </mdb-list-group-item>
                 </mdb-list-group>
@@ -54,7 +51,7 @@ import {
   mdbCardBody,
   mdbIcon,
   mdbBtn,
-  mdbAutocomplete,
+  mdbSelect,
   mdbListGroup,
   mdbListGroupItem,
 
@@ -74,38 +71,39 @@ export default {
     mdbCardBody,
     mdbIcon,
     mdbBtn,
-    mdbAutocomplete,
+    mdbSelect,
     mdbListGroup,
     mdbListGroupItem,
   },
   data() {
     return {
       languages: [],
-      selLanguage: [],
-      selected: '',
-      isSelectedLanguage: false,
+      selectedLanguages: [],
     };
   },
   async mounted() {
     this.$apollo.query({ query: GET_LANGUAGES_QUERY })
       .then((response) => {
         response.data.languages.forEach((language) => {
-          this.languages.push(language.name);
+          this.languages.push({ text: language.name, value: language.name });
         });
       });
     this.languages.sort();
   },
   methods: {
     addLanguage() {
-      if (this.isSelectedLanguage) {
-        this.selLanguage.push(this.selected);
+      const selected = this.selectedLanguage();
+
+      if (selected !== undefined && !this.selectedLanguages.includes(selected)) {
+        this.selectedLanguages.push(selected);
+        this.selectedLanguages.sort();
       }
     },
-    handler(val) {
-      console.log(val);
-      console.log(val.target.value);
-      this.selected = val.target.value;
-      this.isSelectedLanguage = true;
+    selectedLanguage() {
+      const language = this.languages.find(option => option.selected === true);
+      if (language) {
+        return language.value;
+      }
     },
   },
 
